0e6880ae99e33b87e401ea3072d92762fcabd74a
Leon <leonronssin@appnovation.com>
2024-03-07 16:37:52 -0500

    hide settings

---
+++ src/firebase.example.ts
@@ -0,0 +1,16 @@
+import { initializeApp } from 'firebase/app'
+import { getAuth } from 'firebase/auth'
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: '',
+  authDomain: '',
+  projectId: '',
+  storageBucket: '',
+  messagingSenderId: '',
+  appId: ''
+}
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig)
+export const auth = getAuth()
