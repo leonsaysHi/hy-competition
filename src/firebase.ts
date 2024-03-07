d7dbc525aa3589cca1e04983e148696988783d78
Leon <leonronssin@appnovation.com>
2024-03-07 17:27:37 -0500

    config

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,16 +1,21 @@
-import { initializeApp } from 'firebase/app'
-import { getAuth } from 'firebase/auth'
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: 'AIzaSyBab4OZXrKhtb-AfyRBCb8d8BCKsfRG1TY',
-  authDomain: 'hy-competitions.firebaseapp.com',
-  projectId: 'hy-competitions',
-  storageBucket: 'hy-competitions.appspot.com',
-  messagingSenderId: '782030950250',
-  appId: '1:782030950250:web:3fdee3b139e1760f50df13'
-}
+  apiKey: "AIzaSyDb-7CU3jLOTQqeymAUlZYgD4n-hJLJK_4",
+  authDomain: "cityhoops-basketball.firebaseapp.com",
+  projectId: "cityhoops-basketball",
+  storageBucket: "cityhoops-basketball.appspot.com",
+  messagingSenderId: "762961716792",
+  appId: "1:762961716792:web:2e3e752f3f75ebfb9f28cd",
+  measurementId: "G-XRBNCENVWV"
+};
 
 // Initialize Firebase
-export const app = initializeApp(firebaseConfig)
-export const auth = getAuth()
+export const app = initializeApp(firebaseConfig);
+export const analytics = getAnalytics(app);
