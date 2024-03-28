538fee0d3d99c9c6d561cd6b962419965437b8f9
Leon <leonronssin@appnovation.com>
2024-03-27 20:43:51 -0500

    wip

--- src/firebase-storage.ts
+++ src/firebase-storage.ts
@@ -1,21 +1,21 @@
 import { getStorage, ref, uploadBytes, getDownloadURL, deleteObject } from 'firebase/storage'
-
+import { storageTeams, storageSponsors } from './firebase-firestore'
 const storage = getStorage()
 
 export const uploadTeamLogo = async (file: File, name: string) => {
-  const fileRef = ref(storage, 'hy-competition-teams/' + name)
+  const fileRef = ref(storage, storageTeams + '/' + name)
   console.log(file)
   await uploadBytes(fileRef, file)
   const url = await getDownloadURL(fileRef)
   return url
 }
 export const deleteTeamLogo = (name: string) => {
-  const fileRef = ref(storage, 'hy-competition-teams/' + name)
+  const fileRef = ref(storage, storageTeams + '/' + name)
   deleteObject(fileRef)
 }
 
 export const uploadTeamSponsor = (file: File, name: string) => {
-  const fileRef = ref(storage, 'hy-competition-sponsors/' + name)
+  const fileRef = ref(storage, storageSponsors + '/' + name)
   return new Promise((resolve) => {
     uploadBytes(fileRef, file).then(() => {
       getDownloadURL(fileRef).then((url) => {
@@ -25,6 +25,6 @@ export const uploadTeamSponsor = (file: File, name: string) => {
   })
 }
 export const deleteTeamSponsor = (name: string) => {
-  const fileRef = ref(storage, 'hy-competition-sponsors/' + name)
+  const fileRef = ref(storage, storageSponsors + '/' + name)
   deleteObject(fileRef)
 }
