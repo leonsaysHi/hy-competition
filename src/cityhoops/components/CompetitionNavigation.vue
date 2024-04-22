d6e21da8e3ad1d4027721ac2a28ce11739c14f7c
Leon <leonronssin@appnovation.com>
2024-04-22 15:41:36 -0500

    stylings

--- src/cityhoops/components/CompetitionNavigation.vue
+++ src/cityhoops/components/CompetitionNavigation.vue
@@ -10,28 +10,31 @@ const handleAboutUs = () => {
 <template>
   <template v-if="!competitionId"> No competition. </template>
   <template v-else>
-    <nav class="nav flex-column flex-lg-row">
-      <RouterLink
-        :to="{ name: 'competition', params: { competitionId } }"
-        class="nav-link text-white fw-bold"
-        >Inicio</RouterLink
-      >
-      <RouterLink
-        :to="{ name: 'competition-standing', params: { competitionId } }"
-        class="nav-link text-white fw-bold"
-        >Tabla</RouterLink
-      >
-      <RouterLink
-        :to="{ name: 'competition-games', params: { competitionId } }"
-        class="nav-link text-white fw-bold"
-        >Resultados</RouterLink
-      >
-      <a
-        @click="handleAboutUs"
-        class="nav-link text-white fw-bold"
-        >Sobre la liga</a
-      >
-      <SocialsNav class="d-lg-none" />
-    </nav>
+    <div class="hstack align-items-start">
+      <nav class="nav flex-column flex-lg-row">
+        <RouterLink
+          :to="{ name: 'competition', params: { competitionId } }"
+          class="nav-link text-white fw-bold"
+          >Inicio</RouterLink
+        >
+        <RouterLink
+          :to="{ name: 'competition-standing', params: { competitionId } }"
+          class="nav-link text-white fw-bold"
+          >Tabla</RouterLink
+        >
+        <RouterLink
+          :to="{ name: 'competition-games', params: { competitionId } }"
+          class="nav-link text-white fw-bold"
+          >Resultados</RouterLink
+        >
+        <a
+          @click="handleAboutUs"
+          class="nav-link text-white fw-bold"
+          >Sobre la liga</a
+        >
+      </nav>
+      <div class="ms-auto d-lg-none"><SocialsNav /></div>
+    </div>
+    
   </template>
 </template>
