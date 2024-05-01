c1e47bf3ef044d2d75b8a3ed99b986c111ccb7dc
Leon <leonronssin@appnovation.com>
2024-04-30 19:12:24 -0500

    ranking view

--- src/cityhoops/views/RankingsView.vue
+++ src/cityhoops/views/RankingsView.vue
@@ -1,25 +1,14 @@
 <script lang="ts" setup>
 import { useRoute } from 'vue-router'
 import SpinnerComp from '@/components/SpinnerComp.vue'
-import DropdownComp from '@/components/DropdownComp.vue'
-import RadioGroupComp from '@/components/RadioGroupComp.vue'
 import useCompetition from '@/composable/useCompetition'
 import CompetitionStanding from '@/components/competitions/CompetitionStanding.vue'
 import CompetitionRanking from '@/components/competitions/CompetitionRanking.vue'
 import ViewHero from '../components/layout/ViewHero.vue'
-import useCompetitionPhasesGroups from '@/composable/useCompetitionPhasesGroups'
 
 const route = useRoute()
 const { competitionId } = route.params as { competitionId: string }
-const { isReady } = useCompetition(competitionId)
-const {
-  selectedPhaseIdx,
-  selectedPhase,
-  phasesOptions,
-  selectedGroupIdx,
-  selectedGroup,
-  groupsOptions
-} = useCompetitionPhasesGroups()
+const { isReady, competitionRankings, competitionStandings } = useCompetition(competitionId)
 </script>
 <template>
   <div>
@@ -29,29 +18,11 @@ const {
     <template v-else>
       <ViewHero>
         <h1>Tabla y estadística</h1>
-        <template #nav>
-          <template v-if="phasesOptions && phasesOptions.length > 1">
-            <DropdownComp
-              v-model="selectedPhaseIdx"
-              :options="phasesOptions"
-              variant="light"
-              size="lg"
-              class="fw-bold fz-5"
-              :disabled="phasesOptions.length === 1"
-            />
-          </template>
-          <template v-if="groupsOptions && groupsOptions.length > 1">
-            <RadioGroupComp v-model="selectedGroupIdx" :options="groupsOptions" buttons />
-          </template>
-        </template>
       </ViewHero>
-
-      <template v-if="selectedPhase && selectedGroup">
-        <h2>Tabla</h2>
-        <CompetitionStanding :value="selectedGroup.standing" />
-        <h2>Líderes por categorías</h2>
-        <CompetitionRanking :value="selectedGroup.ranking" :limit="25" />
-      </template>
+      <h2>Tabla</h2>
+      <CompetitionStanding :value="competitionStandings" />
+      <h2>Líderes por categorías</h2>
+      <CompetitionRanking :value="competitionRankings" :limit="25" />
     </template>
   </div>
 </template>
